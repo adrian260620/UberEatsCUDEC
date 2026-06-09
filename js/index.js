d84b6e2235449332ad31192f80a8c5e622323336
adrian260620 <aemp030409@gmail.com0>
2026-06-08 21:27:51 -0600

    Los platillos ya aparecen el pagina de una forma agradable

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let contenido = "";
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
@@ -6,3 +7,21 @@ document.addEventListener('DOMContentLoaded', function() {
   const forms = document.querySelectorAll('.side-form');
   M.Sidenav.init(forms, {edge: 'left'});
 });
+
+function mostrarplatillo(platillo, id) {
+  contenido = `
+  <div class='card-pannel recipe white row'>
+  <div class='recipe-details'>
+    <div class='recipe-tittle'>
+      ${platillo.nombre}
+      </div>
+      <div class='recipe.ingredients'>
+        ${platillo.ingredientes}
+        </div>
+
+
+  </div>
+  </div>
+  `;
+  document.querySelector(".recipes").innerHTML += contenido;
+};
